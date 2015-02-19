7f0dc7357b6efe3a56073fdbddd135f2ef86ffd5
camarello <camarello@student.dwc.edu>
2015-02-19 17:40:35 -0500

    Created a Form with Buttons and PictureBox

--- karel/karel/MyForm.h
+++ karel/karel/MyForm.h
@@ -15,15 +15,17 @@ namespace Project1 {
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
 	private:
-		///////////////////// Put Graphics Here /////////////////////////
+		///////////////////// Graphics //////////////////////////////////////////////////
+		private: System::Windows::Forms::PictureBox^  pictureBox1;
+		private: System::Windows::Forms::Button^  button1;
 		Graphics^ g;
 		Bitmap^ KarelLeftbmp = gcnew Bitmap("KarelGraphics/KarelLeft.bmp"); 
 		Bitmap^ KarelRightbmp = gcnew Bitmap("KarelGraphics/KarelRight.bmp");
-		Bitmap^ KarelRightbmp = gcnew Bitmap("KarelGraphics/KarelUp.bmp");
-		Bitmap^ KarelRightbmp = gcnew Bitmap("KarelGraphics/KarelUp.bmp");
-		Bitmap^ KarelRightbmp = gcnew Bitmap("KarelGraphics/KarelUp.bmp");
-		Bitmap^ KarelRightbmp = gcnew Bitmap("KarelGraphics/KarelUp.bmp");
-
+		Bitmap^ KarelUpbmp = gcnew Bitmap("KarelGraphics/KarelUp.bmp");
+		Bitmap^ KarelDownbmp = gcnew Bitmap("KarelGraphics/KarelDown.bmp");
+		Bitmap^ KarelWallBlockbmp = gcnew Bitmap("KarelGraphics/KarelWallBlock.bmp");
+		Bitmap^ KarelBeeperbmp = gcnew Bitmap("KarelGraphics/KarelBeeper.bmp");
+		//////////////////////////////////////////////////////////////////////////////////
 
 	public:
 		MyForm(void)
@@ -59,21 +61,48 @@ namespace Project1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Location = System::Drawing::Point(14, 15);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(400, 400);
+			this->pictureBox1->TabIndex = 0;
+			this->pictureBox1->TabStop = false;
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(443, 178);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(190, 90);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"START";
+			this->button1->UseVisualStyleBackColor = true;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 262);
+			this->ClientSize = System::Drawing::Size(674, 430);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->pictureBox1);
 			this->Name = L"MyForm";
-			this->Text = L"MyForm";
+			this->Text = L"Karel the Robot";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
-	private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
+	private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) 
+	{
+
 	}
 	};
 }
